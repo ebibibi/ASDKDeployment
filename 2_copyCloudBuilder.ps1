7875a1f8d54cd528435876825c779e2924e4f930
Masahiko Ebisuda <ebibibi@gmail.com>
2018-10-03 23:03:06 +0900

    re-added share creation logic.

--- 2_copyCloudBuilder.ps1
+++ 2_copyCloudBuilder.ps1
@@ -16,6 +16,8 @@ if($null -eq $session) {
 $localVHDPath = "C:\ASDK\AzureStackDevelopmentKit\CloudBuilder.vhdx"
 $localVHDFolderPath = "C:\ASDK\AzureStackDevelopmentKit"
 $remoteVHDPath = "D:\ASDK\CloudBuilder.vhdx"
+$remoteASDKPath = "D:\ASDK\"
+$remoteShareName = "ASDK"
 
 # Compare file hash
 Write-Output "Get hash value of local file."
@@ -33,9 +35,18 @@ Write-Output $remoteHash.Hash
 If ($localHash.Hash -ne $remoteHash.Hash) {
     # Copy CloudBuilder.vhdx to ASDK Host
     Write-Output "robocopy CloudBuilder.vhdx to ASDK Host"
+    Invoke-Command $session -ScriptBlock {
+        $ASDKShare = Get-SmbShare -Name $using:remoteShareName -ErrorAction SilentlyContinue
+        if($null -eq $ASDKShare) {
+            New-SmbShare -Name ASDK -Path $using:remoteASDKPath -FullAccess Everyone
+        }        
+    }
+
     if (-not (Test-Path z:)) {
         New-PSDrive -Name z -PSProvider FileSystem -Root \\$env:ASDKHostIP\$remoteShareName -Credential $credential -Persist
     }
+
+    New-PSDrive -Name z -PSProvider FileSystem -Root \\$env:ASDKHostIP\$remoteShareName -Credential $credential -Persist
     $scriptblock = {robocopy $localVHDFolderPath z:\ *.vhdx /r:1 /w:1 /z }
     . $scriptblock
 
