a03248f868baabdc6f80c1d213f24427764f4ef1
Masahiko Ebisuda <ebibibi@gmail.com>
2018-10-03 18:10:47 +0900

    modify typo.

--- 2_copyCloudBuilder.ps1
+++ 2_copyCloudBuilder.ps1
@@ -17,12 +17,18 @@ $remoteVHDPath = "D:\ASDK\CloudBuilder.vhdx"
 $localHash = Get-FileHash $localVHDPath
 $remoteHash = Invoke-command -Session $session -scriptblock { Get-FileHash $using:remoteVHDPath }
 
+Write-Output "localhash"
+Write-Output $localHash.Hash
+
+Write-Output "remotehash"
+Write-Output $remoteHash.Hash
+
 If ($localHash.Hash -ne $remoteHash.Hash) {
   # Copy CloudBuilder.vhdx to ASDK Host
-  Write-Verbose "Copy CloudBuilder.vhdx to ASDK Host"
-  Copy-Item -ToSession $session $localvhd -Destination $remoteVHDPath
+  Write-Output "Copy CloudBuilder.vhdx to ASDK Host"
+  Copy-Item -ToSession $session $localvhdPath -Destination $remoteVHDPath
   } else {
-    Write-Verbose "Copying CloudBuilder.vhd is canceled because Hash value is same."
+    Write-Output "Copying CloudBuilder.vhd is canceled because Hash value is same."
     }
   
 $session | Remove-PSSession
