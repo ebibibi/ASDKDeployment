b13e4e809bbc9cbbac6aa2e19e96f03d822ab7dd
Masahiko Ebisuda <ebibibi@gmail.com>
2018-10-03 23:59:07 +0900

    change boot device to physical disks.

--- 3_changeBootDevicetoPhysicalDisks.ps1
+++ 3_changeBootDevicetoPhysicalDisks.ps1
@@ -2,15 +2,18 @@ Param(
  [String]$ASDKAdminUserPassword
 )
 
-# Open remote session to ASDK Host
+# Credential to ASDK Host
 $password = ConvertTo-SecureString -String $ASDKAdminUserPassword -AsPlainText -Force
 $credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $env:ASDKAdminUserName, $password
 
-# Boot from Original Disk
+# Boot from Physical Disks
 $guid = $env:HDDBootGUID
 Invoke-Command -ComputerName $env:ASDKHostIP -Credential $credential -ScriptBlock {
     bcdedit /default $using:guid
+    shutdown -r
 }
+
+# Wait for reboot
 
 
 #
