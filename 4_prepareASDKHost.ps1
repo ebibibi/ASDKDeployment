8b946b5da6524665f34785e281c0cb883a618934
Masahiko Ebisuda <ebibibi@gmail.com>
2018-10-04 01:08:09 +0900

    create new file.

---
+++ 4_prepareASDKHost.ps1
@@ -0,0 +1,37 @@
+Param(
+ [String]$ASDKAdminUserPassword
+)
+
+# Credential to ASDK Host
+$password = ConvertTo-SecureString -String $ASDKAdminUserPassword -AsPlainText -Force
+$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $env:ASDKAdminUserName, $password
+
+# Wait for reboot
+
+
+#If (Test-Path "C:\CloudBuilder.vhdx") {
+#    If (Test-Path "C:\ReBuildASDK\VHDXDownload\Azure Stack Development Kit\CloudBuilder.vhdx") {
+#        Remove-Item "C:\CloudBuilder.vhdx" -Force
+#    }
+#}
+#Move-Item "C:\ReBuildASDK\VHDXDownload\Azure Stack Development Kit\CloudBuilder.vhdx" C:\
+
+# Variables
+#$Uri = 'https://raw.githubusercontent.com/Azure/AzureStack-Tools/master/Deployment/asdk-installer.ps1'
+#
+#$LocalPath = 'C:\AzureStack_Installer'
+#
+#If (!(Test-Path $LocalPath)){
+#    # Create folder
+#    New-Item $LocalPath -Type directory
+#}
+
+# Enforce usage of TLSv1.2 to download from GitHub
+#[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Download file
+#Invoke-WebRequest $uri -OutFile ($LocalPath + '\' + 'asdk-installer.ps1')
+
+# StartScript
+#Set-Location $LocalPath
+#.\asdk-installer.ps1
