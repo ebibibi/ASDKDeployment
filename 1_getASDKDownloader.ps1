91e9ae8df3a1d8279231dbb5e17d747ee3d2036c
Masahiko Ebisuda <ebibibi@gmail.com>
2018-10-03 12:03:22 +0900

    set path to 'C:/ASDK/azurestackdevkitdownloader.exe'

--- 1_getASDKDownloader.ps1
+++ 1_getASDKDownloader.ps1
@@ -1,6 +1,4 @@
 # https://docs.microsoft.com/ja-jp/azure/azure-stack/azure-stack-deploy-overview
 
-[System.IO.Path]::GetTempPath() | Tee-Object  -Variable installerPath
-$installer = ($installerPath + "AzureStackDownloader.exe")
-Write-Verbose "Installer path : $installer"
-Invoke-WebRequest -Uri https://aka.ms/azurestackdevkitdownloader -OutFile $installer -Verbose
+$downloaderPath = 'C:/ASDK/azurestackdevkitdownloader.exe'
+Invoke-WebRequest -Uri https://aka.ms/azurestackdevkitdownloader -OutFile $downloaderPath -Verbose
