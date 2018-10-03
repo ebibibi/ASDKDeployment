d6d5d0e7f17a645f2b16380a637bc558ceffe580
Masahiko Ebisuda <ebibibi@gmail.com>
2018-10-03 15:04:50 +0900

    added 2nd step.

--- 1_getASDKDownloader.ps1
+++ 1_getASDKDownloader.ps1
@@ -1,8 +1,3 @@
-Param(
- [SecureString]$ASDKAdminUserPassword
-)
-
-
 $downloaderPath = "C:/ASDK/"
 $filename = "azurestackdevkitdownloader.exe"
 If (!(Test-Path $downloaderPath)) {
@@ -13,10 +8,5 @@ Invoke-WebRequest -Uri https://aka.ms/azurestackdevkitdownloader -OutFile ($down
 
 Write-Output ($env:ASDKAdminUserName)
 Write-Output ($ASDKAdminUserPassword)
-
-$Ptr = [System.Runtime.InteropServices.Marshal]::SecureStringToCoTaskMemUnicode($ASDKAdminUserPassword)
-$result = [System.Runtime.InteropServices.Marshal]::PtrToStringUni($Ptr)
-[System.Runtime.InteropServices.Marshal]::ZeroFreeCoTaskMemUnicode($Ptr)
-$result
 
 
--- 2_prepareHost.ps1
+++ 2_prepareHost.ps1
@@ -1,32 +1,42 @@
-Write-Host ($env:ASDKAdminUserName)
-Write-Output ($env:ASDKAdminUserName)
+Param(
+ [String]$ASDKAdminUserPassword
+)
+
+# Add to the TrustedHosts
+Set-Item -Path WSMan:\localhost\Client\TrustedHosts -Value $($env:ASDKHostIP) -force
 
 # Copy CloudBuilder.vhdx to Host root
+$password = ConvertTo-SecureString -String $ASDKAdminUserPassword -AsPlainText -Force
+$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $env:ASDKAdminUserName, $password
+$session = New-PSSession $env:ASDKHostIP -Credential $credential
+Copy-Item -ToSession $session C:\test.txt -Destination C:\test.txt
 
 
-If (Test-Path "C:\CloudBuilder.vhdx") {
-    If (Test-Path "C:\ReBuildASDK\VHDXDownload\Azure Stack Development Kit\CloudBuilder.vhdx") {
-        Remove-Item "C:\CloudBuilder.vhdx" -Force
-    }
-}
-Move-Item "C:\ReBuildASDK\VHDXDownload\Azure Stack Development Kit\CloudBuilder.vhdx" C:\
+
+#
+#If (Test-Path "C:\CloudBuilder.vhdx") {
+#    If (Test-Path "C:\ReBuildASDK\VHDXDownload\Azure Stack Development Kit\CloudBuilder.vhdx") {
+#        Remove-Item "C:\CloudBuilder.vhdx" -Force
+#    }
+#}
+#Move-Item "C:\ReBuildASDK\VHDXDownload\Azure Stack Development Kit\CloudBuilder.vhdx" C:\
 
 # Variables
-$Uri = 'https://raw.githubusercontent.com/Azure/AzureStack-Tools/master/Deployment/asdk-installer.ps1'
-
-$LocalPath = 'C:\AzureStack_Installer'
-
-If (!(Test-Path $LocalPath)){
-    # Create folder
-    New-Item $LocalPath -Type directory
-}
+#$Uri = 'https://raw.githubusercontent.com/Azure/AzureStack-Tools/master/Deployment/asdk-installer.ps1'
+#
+#$LocalPath = 'C:\AzureStack_Installer'
+#
+#If (!(Test-Path $LocalPath)){
+#    # Create folder
+#    New-Item $LocalPath -Type directory
+#}
 
 # Enforce usage of TLSv1.2 to download from GitHub
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+#[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 # Download file
-Invoke-WebRequest $uri -OutFile ($LocalPath + '\' + 'asdk-installer.ps1')
+#Invoke-WebRequest $uri -OutFile ($LocalPath + '\' + 'asdk-installer.ps1')
 
 # StartScript
-Set-Location $LocalPath
-.\asdk-installer.ps1
+#Set-Location $LocalPath
+#.\asdk-installer.ps1
