cd380bf9afd487cbb6a7cbc770d88c3046ee0797
ibeonlyone <ibeonlyone@gmail.com>
2018-10-29 20:39:02 +0900

    add Invoke-Command.

--- 4_prepareASDKHost.ps1
+++ 4_prepareASDKHost.ps1
@@ -28,29 +28,34 @@ while($null -eq $session) {
 Write-Output "Connected to $hostname !!!"
 
 
-If (Test-Path "C:\CloudBuilder.vhdx") {
-    If (Test-Path "C:\ASDK\Azure Stack Development Kit\CloudBuilder.vhdx") {
-        Remove-Item "C:\CloudBuilder.vhdx" -Force
+Invoke-Command -Session $session -ScriptBlock {
+
+
+    If (Test-Path "C:\CloudBuilder.vhdx") {
+        If (Test-Path "C:\ASDK\Azure Stack Development Kit\CloudBuilder.vhdx") {
+            Remove-Item "C:\CloudBuilder.vhdx" -Force
+        }
     }
+    Move-Item "C:\ASDK\Azure Stack Development Kit\CloudBuilder.vhdx" C:\
+
+    # Variables
+    $Uri = 'https://raw.githubusercontent.com/Azure/AzureStack-Tools/master/Deployment/asdk-installer.ps1'
+
+    $LocalPath = 'C:\AzureStack_Installer'
+
+    If (!(Test-Path $LocalPath)){
+        # Create folder
+        New-Item $LocalPath -Type directory
+    }
+
+    # Enforce usage of TLSv1.2 to download from GitHub
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+    # Download file
+    Invoke-WebRequest $uri -OutFile ($LocalPath + '\' + 'asdk-installer.ps1')
+
+    # StartScript
+    Set-Location $LocalPath
+    #.\asdk-installer.ps1
+
 }
-Move-Item "C:\ASDK\Azure Stack Development Kit\CloudBuilder.vhdx" C:\
-
-# Variables
-$Uri = 'https://raw.githubusercontent.com/Azure/AzureStack-Tools/master/Deployment/asdk-installer.ps1'
-
-$LocalPath = 'C:\AzureStack_Installer'
-
-If (!(Test-Path $LocalPath)){
-    # Create folder
-    New-Item $LocalPath -Type directory
-}
-
-# Enforce usage of TLSv1.2 to download from GitHub
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
-# Download file
-Invoke-WebRequest $uri -OutFile ($LocalPath + '\' + 'asdk-installer.ps1')
-
-# StartScript
-Set-Location $LocalPath
-#.\asdk-installer.ps1
